4106dd9c2eacb7b3cfb7d066977ca26509ec1985
sevenns <a.sevenns@gmail.com>
2017-01-10 23:09:51 +0300

    add svg sprites/some corrections of structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,11 @@ var gulp =					require('gulp'),
 		concat =				require('gulp-concat'),
 		sync =					require('browser-sync').create(),
 		imagemin =			require('gulp-imagemin'),
-		remove =				require('gulp-clean');
+		remove =				require('gulp-clean'),
+		svgsprite =			require('gulp-svg-sprite'),
+		svgmin =				require('gulp-svgmin'),
+		cheerio =				require('gulp-cheerio'),
+		replace =				require('gulp-replace');
 
 var sourceDir = "./src/",
 		distDir = "./dist/";
@@ -41,7 +45,8 @@ gulp.task('pug', function() {
 
 gulp.task('scripts-libs-minify', function() {
 	return gulp.src([
-		sourceDir + "libs/jquery/dist/jquery.min.js"
+		sourceDir + "libs/jquery/dist/jquery.min.js",
+		sourceDir + "libs/svg4everybody/dist/svg4everybody.min.js"
 		//add some libs
 	])
 	.pipe(concat('libs.min.js'))
@@ -68,20 +73,52 @@ gulp.task('img-delete', function() {
 });
 
 gulp.task('img-minify', ['img-delete'], function() {
-	return gulp.src(sourceDir + "img/**/*.+(png|jpg|gif|svg|ico)")
+	return gulp.src(sourceDir + "img/**/*.+(png|jpg|gif|ico)")
 	.pipe(imagemin())
 	.pipe(gulp.dest(distDir + "img/"));
 });
 
-gulp.task('watch', ['sass', 'pug', 'scripts-libs-minify', 'scripts-minify', 'img-minify', 'sync'], function() {
+gulp.task('svg-sprite', function() {
+	return gulp.src(sourceDir + "img/icons/*.svg")
+	.pipe(svgmin({
+		js2svg: { pretty: true }
+	}))
+	.pipe(cheerio({
+		run: function($) {
+			$('[fill]').removeAttr('fill');
+			$('[stroke]').removeAttr('stroke');
+			$('[style]').removeAttr('style');
+		},
+		parserOptions: { xmlMode: true }
+	}))
+	.pipe(replace('&gt;', '>'))
+	.pipe(svgsprite({
+		mode: {
+			symbol: {
+				sprite: "../sprites.svg",
+				render: {
+					scss: {
+						dest: "../../../src/scss/_sprites.scss",
+						template: sourceDir + "scss/_sprites_layout.scss"
+					}
+				}
+			}
+		}
+	}))
+	.pipe(gulp.dest(distDir + "img/"));
+});
+
+gulp.task('watch', ['sass', 'pug', 'scripts-libs-minify', 'scripts-minify', 'img-minify', 'svg-sprite', 'sync'], function() {
 	gulp.watch(sourceDir + "scss/**/*.scss", ['sass']);
 	gulp.watch(sourceDir + "pug/**/*.pug", ['pug']);
 	gulp.watch(sourceDir + "scripts/**/*.js", ['scripts-minify']);
 	gulp.watch(sourceDir + "libs/**/*.js", ['scripts-libs-minify']);
-	gulp.watch(sourceDir + "img/**/*.+(png|jpg|gif|svg)", ['img-minify']);
+	gulp.watch(sourceDir + "img/**/*.+(png|jpg|gif)", ['img-minify']);
+	gulp.watch(sourceDir + "img/icons/*.svg", ['svg-sprite']);
 	gulp.watch(distDir + "*.html").on('change', sync.reload);
 	gulp.watch(sourceDir + "scripts/**/*.js").on('change', sync.reload);
-	gulp.watch(distDir + "img/**/*.+(png|jpg|gif|svg)").on('change', sync.reload);
+	gulp.watch(sourceDir + "img/icons/*.svg").on('change', sync.reload);
+	gulp.watch(distDir + "img/**/*.+(png|jpg|gif)").on('change', sync.reload);
 });
 
 gulp.task('start', ['watch'], function() {
---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+// Code here
+
+
+// For correct load svg files if it's old browser (or not support)
+
+function correctLoadSvg() {
+  svg4everybody({});
+}
+
+$(document).ready(correctLoadSvg());
