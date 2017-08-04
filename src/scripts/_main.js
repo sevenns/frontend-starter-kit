a232275798ab6af6a2db0f06713df8c0874eece0
Apesong <apesong@Apesongs-MacBook-Pro.local>
2017-08-04 21:25:08 +0300

    Settings have been added. Some restructure :). Small fix for preloader.

--- src/scripts/_main.js
+++ src/scripts/_main.js
@@ -2,15 +2,21 @@
 
 import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
+import settings from './_settings';
 
 $(document).ready(() => {
 
+  
+  // Preloader
+  if(settings.preloader.enable) 
+    $('.preloader').fadeOut(settings.preloader.delay);
+  else
+    $('.preloader').remove();
 
-  // Disabling the preloader
-  $('#preloader').fadeOut(300);
 
   // For correct load svg for all browsers
   svg4everybody();
   
+
 
 });
---
+++ src/scripts/_settings.js
@@ -0,0 +1,10 @@
+const settings = {
+  
+  preloader: {
+    enable: true,
+    delay: 300
+  }
+
+};
+
+export default settings;
