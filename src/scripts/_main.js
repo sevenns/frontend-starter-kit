d4850c5b01385d0c66c4d8e406f5654dd100b3fe
Apesong <apesong@Apesongs-MacBook-Pro.local>
2017-08-09 14:37:24 +0300

    preloader fix

--- src/scripts/_main.js
+++ src/scripts/_main.js
@@ -4,14 +4,17 @@ import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
 import settings from './_settings';
 
+// Preloader
+if(settings.preloader.enable) {
+  $(window).on('load', function() {
+    $('.preloader').delay(500).fadeOut(settings.preloader.delay);
+  });
+}
+else
+  $('.preloader').remove();
+
 $(document).ready(() => {
 
-  
-  // Preloader
-  if(settings.preloader.enable) 
-    $('.preloader').fadeOut(settings.preloader.delay);
-  else
-    $('.preloader').remove();
 
 
   // For correct load svg for all browsers
